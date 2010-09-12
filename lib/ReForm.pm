48218c5c617105a43eec8cc1d8e85d18f8b03b99
Steven Haryanto (on Asus EEEPC) <stevenharyanto@gmail.com>
2010-09-12 10:04:19 +0700

    first import, skeleton

---
+++ lib/ReForm.pm
@@ -0,0 +1,50 @@
+package ReForm;
+# ABSTRACT: Yet another form handling module
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+L<ReForm> is yet another form handling module/framework. Features:
+
+=over 4
+
+=item * Specification using L<Data::Schema> schema
+
+=item * Clearer separation between semantic (e.g. boolean data type) and presentation (e.g. checkbox or radio Yes/No or select Yes/No)
+
+=item * Subforms
+
+=item * Translation
+
+=item * Renderers for console, TUI (ncurses), HTML
+
+=item * HTML renderer: CSS, AJAX, jQuery, templates, multipage form
+
+=back
+
+=cut
+
+use 5.010;
+use strict;
+use warnings;
+
+use Any::Moose;
+#use Data::Schema;
+
+=head1 METHODS
+
+=head1 FAQ
+
+=head1 SEE ALSO
+
+Other manuals: L<ReForm::Manual::Tutorial>
+
+Modules used: L<Data::Schema>
+
+Other form handling frameworks/modules: XXX
+
+=cut
+
+1;
