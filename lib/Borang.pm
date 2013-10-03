27a59e11000863cc25ce2e16b35149451805d73c
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-10-03 19:57:31 +0700

    Release v0.00

---
+++ lib/Borang.pm
@@ -0,0 +1,22 @@
+package Borang;
+
+use 5.010001;
+
+# VERSION
+
+1;
+# ABSTRACT: Yet another form handling module
+
+=head1 DESCRIPTION
+
+NOT YET IMPLEMENTED.
+
+
+=head1 FAQ
+
+=head2 Why the name?
+
+Borang means form in Indonesian and is currently seldom used in daily
+conversations.
+
+=cut
