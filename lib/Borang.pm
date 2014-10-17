decf0d98c1b0b4a835e26c47288a1c79a4c37096
perlancar (@pc-office) <perlancar@gmail.com>
2014-10-17 09:38:59 +0700

    Fill in Abstract and Description for main module

--- lib/Borang.pm
+++ lib/Borang.pm
@@ -1,15 +1,22 @@
 package Borang;
 
-use 5.010001;
-
+# DATE
 # VERSION
 
+use 5.010001;
+
 1;
-# ABSTRACT: Yet another form handling module
+# ABSTRACT: Rinci-based form framework
 
 =head1 DESCRIPTION
 
-NOT YET IMPLEMENTED.
+B<WORK IN PROGRESS, SOME THINGS ARE NOT YET IMPLEMENTED>.
+
+Borang is a form framework that is function-oriented (and not object-oriented,
+on purpose). In Borang, form submission is eventually sent to a function and
+vice versa form is generated from L<Rinci> function metadata.
+
+Borang is environment-agnostic (can generate HTML forms as well as CLI/CUI/GUI).
 
 
 =head1 FAQ
@@ -18,5 +25,10 @@ NOT YET IMPLEMENTED.
 
 Borang means form in Indonesian and is currently seldom used in daily
 conversations.
+
+
+=head1 SEE ALSO
+
+L<Rinci>
 
 =cut
